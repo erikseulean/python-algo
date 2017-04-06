eb5d5d93cd020ec60cc28c6169f1427d0e60e224
Erik-Cristian Seulean <Erik-Cristian Seulean>
2017-04-06 19:34:30 +0100

    permutations

--- Problems/permutations.py
+++ Problems/permutations.py
@@ -12,4 +12,40 @@ def permute(s, mine, used):
             mine = mine[:-1]
 
 
-permute('broasc', '', set())
+# permute('broasc', '', set())
+
+def is_empty(wordmap):
+    for key in wordmap:
+        if wordmap[key] is not 0:
+            return False
+    return True
+
+def word_permutation(word_map, current, perms):
+    if is_empty(word_map):
+        perms.append(current)
+        return
+    
+    for key in word_map:
+        if word_map[key] is not 0:
+            word_map[key] -= 1
+            word_permutation(word_map, current + key, perms)
+            word_map[key] += 1
+
+def word_map(word):
+    _map = dict()
+    for letter in word:
+        if letter in _map:
+            _map[letter] += 1
+        else:
+            _map[letter] = 1
+    return _map
+
+def permute_unique(word):
+    data = []
+    word_permutation(word_map(word), '', data)
+    print(data)
+
+permute_unique('abra')
+
+
+
